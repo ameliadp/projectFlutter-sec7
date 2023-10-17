f37bd182450616264d0daae7e6fee7dd67fa421b
amelia <ameliadarmonoputri06@gmail.com>
2023-10-17 16:26:33 +0700

    updateProgres1

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,103 +1,110 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
+  import 'package:get/get.dart';
 
-import '../controllers/home_controller.dart';
+  import '../controllers/home_controller.dart';
 
-class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+  class HomeView extends GetView<HomeController> {
+    final HomeController homeController = Get.put(HomeController());
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          //Logo&Title
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  'assets/images/logoPurple.png',
+                  width: 40,
+                  height: 40,
+                ),
+              ),
+              const Text(
+                'Fake Store',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    color: Color(0xff802c6e)),
+              ),
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                'assets/images/logoPurple.png',
-                width: 40,
-                height: 40,
+            SingleChildScrollView(
+              //searchBar&Filter
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    width: 230,
+                    height: 50,
+                    child: TextField(
+                      autocorrect: false,
+                      textInputAction: TextInputAction.search,
+                      decoration: InputDecoration(
+                          isCollapsed: true,
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                          labelText: 'Search Product',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Poppins Light',
+                            color: Colors.grey,
+                          ),
+                          suffixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xff802c6e),
+                            ),
+                            borderRadius: BorderRadius.circular(30),
+                          )),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    width: 100,
+                    height: 50,
+                    child: TextField(
+                      autocorrect: false,
+                      textInputAction: TextInputAction.search,
+                      decoration: InputDecoration(
+                          isCollapsed: true,
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                          labelText: 'Filter',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Poppins Light',
+                            color: Colors.grey,
+                          ),
+                          suffixIcon: Icon(Icons.filter_alt_outlined),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xff802c6e),
+                            ),
+                            borderRadius: BorderRadius.circular(30),
+                          )),
+                    ),
+                  )
+                ],
               ),
             ),
-            const Text(
-              'Fake Store',
-              style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 20,
-                  color: Color(0xff802c6e)),
-            ),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  width: 250,
-                  child: TextField(
-                    autocorrect: false,
-                    textInputAction: TextInputAction.search,
-                    decoration: InputDecoration(
-                        isCollapsed: true,
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        labelText: 'Search Product',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Poppins Light',
-                          color: Colors.grey,
-                        ),
-                        suffixIcon: Icon(Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xff802c6e),
-                          ),
-                          borderRadius: BorderRadius.circular(30),
-                        )),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  width: 100,
-                  child: TextField(
-                    autocorrect: false,
-                    textInputAction: TextInputAction.search,
-                    decoration: InputDecoration(
-                        isCollapsed: true,
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        labelText: 'Filter',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Poppins Light',
-                          color: Colors.grey,
-                        ),
-                        suffixIcon: Icon(Icons.filter_alt_outlined),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xff802c6e),
-                          ),
-                          borderRadius: BorderRadius.circular(30),
-                        )),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: GridView.builder(
+            //Card GridView
+            Expanded(
+                child: GridView.builder(
+                  key: UniqueKey(),
               padding: EdgeInsets.all(20),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisSpacing: 20,
@@ -106,11 +113,11 @@ class HomeView extends GetView<HomeController> {
               ),
               itemCount: 6,
               itemBuilder: (context, index) {
-                String imageName = 'assets/images/product${index + 1}.png';
+                final product = homeController.products[index];
 
                 return Container(
                   width: 100,
-                  height: 150,
+                  height: 300,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -123,23 +130,119 @@ class HomeView extends GetView<HomeController> {
                       ),
                     ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Image.asset(
-                        imageName,
-                        width: 90,
-                        height: 90,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Image.asset(
+                          product.image,
+                          width: 100,
+                          height: 145,
+                        ),
                       ),
-                    ),
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 7,left: 7),
+                        child: Text(
+                          product.title,
+                          style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontFamily: 'Poppins Bold',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                          maxLines: 1,
+                        ),
+                      ),
+                      SizedBox(height: 6),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 7,left: 7),
+                        child: Text(
+                          product.description,
+                          style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontFamily: 'Poppins Regular',
+                            fontSize: 13,
+                          ),
+                          maxLines: 2,
+                        ),
+                      ),
+                      SizedBox(height: 6),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 7,left: 7),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '\$${product.price.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                fontFamily: 'Poppins Bold',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 7, right: 7),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 16,
+                                  ),
+                            
+                                  Text(
+                                    '${homeController.products[index].rating.rate.toString()}',
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins Regular',
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
+            )),
+            //button Add Product
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(190, 40),
+                    backgroundColor: Color(0xff802c6e),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Add New Product',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.white,
+                        ),
+                      ),
+                      Icon(
+                        Icons.add_circle_outline_rounded,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
   }
-}
